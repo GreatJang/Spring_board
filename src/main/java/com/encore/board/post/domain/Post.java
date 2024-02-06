9e8e9860eb1c98141d2456d56c6e17001b44e39e
GreatJang <1wkdwnsgur@gmail.com>
2024-02-06 17:09:56 +0900

    login-page_update

--- src/main/java/com/encore/board/post/domain/Post.java
+++ src/main/java/com/encore/board/post/domain/Post.java
@@ -1,21 +1,22 @@
 package com.encore.board.post.domain;
 
 import com.encore.board.author.domain.Author;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
+import lombok.*;
 import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UpdateTimestamp;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import javax.persistence.*;
 import java.time.LocalDateTime;
 
 @Entity
 @Getter
-@NoArgsConstructor
 @Builder
 @AllArgsConstructor
+@EntityListeners(AuditingEntityListener.class)
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Post {
 //        author객체의 posts를 초기화시켜준 후,
 //        this.author.getPosts().add(this); // Author posts에 Setter를 사용하지 않고 사용하는 방법
@@ -40,18 +41,14 @@ public class Post {
 
 //    author_id는 DB의 컬럼명, 별다른 옵션없을시 author의 pk에 fk가 설정
 //    post객체 입장에서는 한사람이 여러개 글을 쓸수 있으므로 N:1 ManyToOne으로 설정
-    @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "author_id")
-//    @JoinColumn(nullable=false, name="author_email", referenceColumnName="email")
-    private Author author;
 
-    @CreationTimestamp
-    @Column(columnDefinition = "TIMESTAMP DEFAULT CURRENT_TIMESTAMP")
-    private LocalDateTime createdTime;
+    private String email;
 
-    @UpdateTimestamp
-    @Column(columnDefinition = "TIMESTAMP ON UPDATE CURRENT_TIMESTAMP DEFAULT CURRENT_TIMESTAMP")
-    private LocalDateTime updatedTime;
+    @CreatedDate
+    private LocalDateTime createdAt;
+
+    @LastModifiedDate
+    private LocalDateTime updatedAt;
 
     private String appointment;
 
